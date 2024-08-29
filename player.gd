501f9c29ba479ce2a23d963ef141a8ac21a5705c
Jacob Howell <jhowell45@googlemail.com>
2024-08-29 23:55:17 +0100

    basic movement.

--- player.gd
+++ player.gd
@@ -1,1 +1,7 @@
 extends CharacterBody2D
+
+@export var speed: int
+
+func _physics_process(delta: float) -> void:
+	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
+	move_and_slide()
