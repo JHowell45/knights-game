33a75c1bce9834f2aa57b0fd02687ac9e1a6f3f8
Jacob Howell <jhowell45@googlemail.com>
2024-08-30 00:21:00 +0100

    Sets default animation to Idle.

--- player.gd
+++ player.gd
@@ -2,9 +2,12 @@ extends CharacterBody2D
 
 @export var speed: int
 
-@onready var state = %CharacterState
+@onready var animator: AnimationPlayer = %AnimationPlayer
+@onready var state: Node = %CharacterState
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
 	move_and_slide()
 	print(velocity)
+
+	
