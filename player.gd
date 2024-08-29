a23915c9579ac6204c12e95792b4b62a1586d6e3
Jacob Howell <jhowell45@googlemail.com>
2024-08-30 00:16:45 +0100

    player script has access to the state.

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed: int
 
+@onready var state = %CharacterState
+
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
 	move_and_slide()
