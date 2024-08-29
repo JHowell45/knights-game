39746c3338d47d8afdbdf6420bb6bf6d5d279e17
Jacob Howell <jhowell45@googlemail.com>
2024-08-30 00:06:52 +0100

    display the player velocity.

--- player.gd
+++ player.gd
@@ -5,3 +5,4 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
 	move_and_slide()
+	print(velocity)
