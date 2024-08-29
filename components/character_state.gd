82a94c6fed3700f83c46e87d315151dea1f35832
Jacob Howell <jhowell45@googlemail.com>
2024-08-30 00:32:47 +0100

    setup basic animation states.

--- components/character_state.gd
+++ components/character_state.gd
@@ -10,5 +10,31 @@ enum States {
 
 @export var state: States
 
-func _set_state(state: States, animator: AnimationPlayer):
-	pass
+func _set_state(new_state: States, animator: AnimationPlayer):
+	state = new_state
+	match (state):
+		States.IDLE:
+			idle(animator)
+		States.RUNNING:
+			running(animator)
+		States.ATTACKING:
+			attacking(animator)
+		States.HIT:
+			hit(animator)
+		States.DEAD:
+			dead(animator)
+
+func idle(animator: AnimationPlayer):
+	animator.play("Idle")
+	
+func running(animator: AnimationPlayer):
+	animator.play("Run")
+	
+func attacking(animator: AnimationPlayer):
+	animator.play("Idle")
+	
+func hit(animator: AnimationPlayer):
+	animator.play("Idle")
+	
+func dead(animator: AnimationPlayer):
+	animator.play("Idle")
--- player.gd
+++ player.gd
@@ -3,13 +3,18 @@ extends CharacterBody2D
 @export var speed: int
 
 @onready var animator: AnimationPlayer = %AnimationPlayer
-@onready var state: Node = %CharacterState
+@onready var state = %CharacterState
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
 	move_and_slide()
 	print(velocity)
 	
-	
+	if velocity.is_zero_approx():
+		state._set_state(state.States.IDLE, animator)
+	else:
+		state._set_state(state.States.RUNNING, animator)
+		
+	print(animator.get_queue())
 
 	
