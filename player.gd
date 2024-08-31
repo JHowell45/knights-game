e8537b94651e04c8c756ad2098a50c3fcbc6b5c8
Jacob Howell <jhowell45@googlemail.com>
2024-08-31 02:29:48 +0100

    Adding direction to attack animation.

--- player.gd
+++ player.gd
@@ -10,13 +10,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	print(velocity)
 	
-	if state.state != state.States.ATTACKING:
+	if state.state != state.States.ATTACK:
 		if velocity.is_zero_approx():
 			state._set_state(state.States.IDLE, animator)
 		else:
-			state._set_state(state.States.RUNNING, animator)
+			state._set_state(state.States.RUN, animator)
 	if Input.is_action_pressed("attack"):
-		state._set_state(state.States.ATTACKING, animator)
+		if velocity.x != 0 && velocity.y != 0:
+			state._set_state(state.States.ATTACK, animator, state.Direction.LEFT)
+		elif velocity.y < 0:
+			state._set_state(state.States.ATTACK, animator, state.Direction.UP)
+		elif velocity.y > 0:
+			state._set_state(state.States.ATTACK, animator, state.Direction.DOWN)
+		else:
+			state._set_state(state.States.ATTACK, animator, state.Direction.LEFT)
 		
 	print(animator.get_queue())
 
