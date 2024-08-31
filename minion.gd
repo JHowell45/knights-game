3f03a1f8f29d21bf3b81f72a9a446f2b198b0800
Jacob Howell <jhowell45@googlemail.com>
2024-08-31 23:50:31 +0100

    Updated movement for the minion to follow.

--- minion.gd
+++ minion.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 @onready var state = %MinionState
 @onready var animator: AnimationPlayer = %AnimationPlayer
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
 
 @onready var following: bool = false
 
@@ -14,23 +15,24 @@ const DISTANCE: int = 50
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	if following:
-		var player_pos = player.global_position
-		print("x: %d" % abs(global_position.x - player_pos.x))
-		print("y: %d" % abs(global_position.y - player_pos.y))
-		direction = global_position.direction_to(player_pos)
+		var new_position = player.environment_collision.global_position
+		if (player.direction == player.state.Direction.LEFT):
+			new_position.x += DISTANCE
+		if (player.direction == player.state.Direction.RIGHT):
+			new_position.x -= DISTANCE
+		nav.target_position = new_position
 		
-		if abs(global_position.x - player_pos.x) > DISTANCE and abs(global_position.y - player_pos.y) > DISTANCE:
-			pass
-		elif abs(global_position.x - player_pos.x) > DISTANCE:
-			direction = global_position.direction_to(player_pos)
-			direction.y = 0
-		elif abs(global_position.y - player_pos.y) > DISTANCE:
-			direction = global_position.direction_to(player_pos)
-			direction.x = 0
-		else:
-			direction = Vector2.ZERO
-		velocity = direction * speed * delta
+		if not nav.is_navigation_finished():
+			var current_nav_position = global_position
+			var next_path_position = nav.get_next_path_position()
+			var new_velocity = current_nav_position.direction_to(next_path_position) * speed * delta
+		
+			if nav.avoidance_enabled:
+				nav.set_velocity(new_velocity)
+			else:
+				_on_navigation_agent_2d_velocity_computed(new_velocity)
 		move_and_slide()
+		
 	if velocity.is_zero_approx():
 		state._set_state(state.States.IDLE, animator)
 	else:
@@ -40,3 +42,7 @@ func _physics_process(delta: float) -> void:
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action("left_click") and event.is_pressed() and not following:
 		following = true
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
