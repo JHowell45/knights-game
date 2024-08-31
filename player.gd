c3f22313fbb7101d1b97ef95af93de8481f8db16
Jacob Howell <jhowell45@googlemail.com>
2024-08-31 02:48:48 +0100

    Add attack timer to prevent attack animation clipping.

--- player.gd
+++ player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @onready var animator: AnimationPlayer = %AnimationPlayer
 @onready var state = %CharacterState
 @onready var sprite: Sprite2D = %Sprite2D
+@onready var attack_timer: Timer = %Timer
+
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left", "right", "up", "down") * speed * delta
@@ -16,7 +18,7 @@ func _physics_process(delta: float) -> void:
 			state._set_state(state.States.IDLE, animator)
 		else:
 			state._set_state(state.States.RUN, animator)
-	if Input.is_action_pressed("attack"):
+	if Input.is_action_pressed("attack") and attack_timer.is_stopped():
 		if velocity.x != 0 && velocity.y != 0:
 			state._set_state(state.States.ATTACK, animator, state.Direction.LEFT)
 		elif velocity.y < 0:
@@ -25,6 +27,7 @@ func _physics_process(delta: float) -> void:
 			state._set_state(state.States.ATTACK, animator, state.Direction.DOWN)
 		else:
 			state._set_state(state.States.ATTACK, animator, state.Direction.LEFT)
+		attack_timer.start()
 
 func _handle_sprite_flip():
 	if velocity.x < 0:
