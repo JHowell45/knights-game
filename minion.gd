7ff41e1287c39f6611a347f700d3812f5cab1c54
Jacob Howell <jhowell45@googlemail.com>
2024-08-31 20:47:14 +0100

    basic minion impl.

--- minion.gd
+++ minion.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+@onready var state = %MinionState
 @onready var animator: AnimationPlayer = %AnimationPlayer
 
 func _physics_process(delta: float) -> void:
-	
-	
-	animator.play("Idle")
+	if velocity.is_zero_approx():
+		state._set_state(state.States.IDLE, animator)
+	else:
+		state._set_state(state.States.RUN, animator)
