52afcb56c4d41e6db1954e2de05fe48f87632d8f
Jacob Howell <jhowell45@googlemail.com>
2024-09-03 22:51:01 +0100

    added death signla code.

--- enemies/torch_goblin.gd
+++ enemies/torch_goblin.gd
@@ -4,12 +4,17 @@ signal dead
 
 @export var health: int
 
-func _physics_process(delta: float) -> void:
-	print("health: %d" % health)
-
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	var damage = area.get_parent().attack
 	health -= damage
 	if health <= 0:
 		dead.emit()
-		queue_free()
+
+
+func _on_dead() -> void:
+	var pos = global_position
+	queue_free()
+	const DEATH = preload("res://death.tscn")
+	var death = DEATH.instantiate()
+	death.global_position = pos
+	get_parent().add_child(death)
