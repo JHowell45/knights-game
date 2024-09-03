a738a438c7b8ff1419913eee8fcdda802124f5aa
Jacob Howell <jhowell45@googlemail.com>
2024-09-03 22:40:40 +0100

    take damage and die.

--- enemies/torch_goblin.gd
+++ enemies/torch_goblin.gd
@@ -1,20 +1,15 @@
 extends CharacterBody2D
 
-func _physics_process(delta: float) -> void:
-	pass
+signal dead
 
+@export var health: int
+
+func _physics_process(delta: float) -> void:
+	print("health: %d" % health)
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
-	print("HIT 1")
-
-
-func _on_hurt_box_body_entered(body: Node2D) -> void:
-	print("HIT 2")
-
-
-func _on_hurt_box_mouse_entered() -> void:
-	print("MOUSE")
-
-
-func _on_hurt_box_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print("HIT 3")
+	var damage = area.get_parent().attack
+	health -= damage
+	if health <= 0:
+		dead.emit()
+		queue_free()
