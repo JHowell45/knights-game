52313906a3752d1d4737d8abb1a52061b02a57e8
Jacob Howell <jhowell45@googlemail.com>
2024-09-03 22:31:23 +0100

    signal connection for hurtbox.

--- enemies/torch_goblin.gd
+++ enemies/torch_goblin.gd
@@ -5,4 +5,16 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
-	print("HIT")
+	print("HIT 1")
+
+
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	print("HIT 2")
+
+
+func _on_hurt_box_mouse_entered() -> void:
+	print("MOUSE")
+
+
+func _on_hurt_box_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	print("HIT 3")
