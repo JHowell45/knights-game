41a1edc1d3a1187c1718a89e7d42b39a556978ed
Jacob Howell <jhowell45@googlemail.com>
2024-09-04 23:31:16 +0100

    allow the enemy goblin to spot the player knights.

--- enemies/vision.gd
+++ enemies/vision.gd
@@ -1,5 +1,19 @@
 extends Area2D
 
+signal attack(enemy: CharacterBody2D)
+
+@onready var attacking: bool = false
+@onready var enemy_distance: float = INF
+@onready var enemy: CharacterBody2D
+
+
 func _physics_process(delta: float) -> void:
-	if get_overlapping_bodies():
-		print(get_overlapping_bodies())
+	for body in get_overlapping_bodies():
+		if body.is_in_group("Knights") and body != enemy:
+			var distance = global_position.distance_to(body.global_position)
+			print(distance)
+			if enemy_distance > distance:
+				enemy_distance = distance
+				enemy = body
+	if enemy:
+		attack.emit(enemy)
