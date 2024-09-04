320ac8fa372e42b5eb053fa52d1ff36739a7c922
Jacob Howell <jhowell45@googlemail.com>
2024-09-04 13:37:14 +0100

    clean up old healthbars.

--- components/health.gd
+++ components/health.gd
@@ -2,4 +2,18 @@ extends Node
 
 @export var min_health: int
 @export var max_health: int
-@export var current_health: int
+@export var parent: TextureProgressBar = get_parent()
+
+func _ready() -> void:
+	print(parent)
+	#parent.max_value = maxas_health
+	#parent.min_value = min_health
+
+func _process(delta: float) -> void:
+	pass
+	
+func take_damage(amount: int) -> void:
+	parent.value -= amount
+	
+func heal(amount: int) -> void:
+	parent.value += amount
