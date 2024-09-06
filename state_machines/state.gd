43595db2523c6bb43bbaa328633d6faaa07eef0a
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 20:00:33 +0100

    Creating basic state node.

---
+++ state_machines/state.gd
@@ -0,0 +1,16 @@
+class_name State
+extends Node
+
+signal state_changed(new_state: StringName)
+
+func _enter() -> void:
+	pass
+	
+func _exit() -> void:
+	pass
+
+func _update() -> void:
+	pass
+	
+func _physics_update() -> void:
+	pass
