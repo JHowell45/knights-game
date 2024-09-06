27044acec50886adee60af8c39210ec5160ea0c1
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 23:40:32 +0100

    docs.

--- state.gd
+++ state.gd
@@ -3,14 +3,18 @@ extends Node
 
 signal state_changed(new_state: StringName)
 
+# Actions to perform when entering this state.
 func _enter() -> void:
 	pass
-	
+
+# Actions to perform when exiting this state.
 func _exit() -> void:
 	pass
 
+# Actions to perform when updating the frame in this state.
 func _update(delta: float) -> void:
 	pass
-	
+
+# Actions to perform when updating the physics for a frame in this state.
 func _physics_update(delta: float) -> void:
 	pass
