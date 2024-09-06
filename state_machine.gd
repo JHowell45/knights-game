597376957171cdf3288fb4122b148066e0d286aa
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 20:15:25 +0100

    impl state change and adding methods for update and update for physics.

--- state_machine.gd
+++ state_machine.gd
@@ -13,6 +13,19 @@ func _ready() -> void:
 		else:
 			push_warning("Invalid node! Should be of type State!")
 	current_state._enter()
-			
-func _on_state_changed() -> void:
+	
+func _process(delta) -> void:
 	pass
+	
+func _physics_process(delta) -> void:
+	pass
+			
+func _on_state_changed(new_state: StringName) -> void:
+	var next_state = states.get(new_state)
+	
+	if next_state and next_state != current_state:
+		current_state._exit()
+		next_state._enter()
+		current_state = next_state
+	else:
+		push_error(("Invalid state: '%s'" % new_state))
