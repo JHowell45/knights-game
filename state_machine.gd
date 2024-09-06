c1218b7a1cf39a53133f1b8c0766ac514593218b
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 20:08:57 +0100

    basic state machine impl.

--- state_machine.gd
+++ state_machine.gd
@@ -1,1 +1,18 @@
+class_name StateMachine
 extends Node
+
+@export var current_state: State
+
+var states : Dictionary = {}
+
+func _ready() -> void:
+	for state in get_children():
+		if state is State:
+			states[state.name] = state
+			state.state_changed.connect(_on_state_changed)
+		else:
+			push_warning("Invalid node! Should be of type State!")
+	current_state._enter()
+			
+func _on_state_changed() -> void:
+	pass
