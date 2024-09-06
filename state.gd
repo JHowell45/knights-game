6155b10a314d5ea40dabed0f07e8d798adf1087b
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 23:42:13 +0100

    Update method names.

--- state.gd
+++ state.gd
@@ -4,17 +4,17 @@ extends Node
 signal state_changed(new_state: StringName)
 
 # Actions to perform when entering this state.
-func _enter() -> void:
+func enter() -> void:
 	pass
 
 # Actions to perform when exiting this state.
-func _exit() -> void:
+func exit() -> void:
 	pass
 
 # Actions to perform when updating the frame in this state.
-func _update(delta: float) -> void:
+func update(delta: float) -> void:
 	pass
 
 # Actions to perform when updating the physics for a frame in this state.
-func _physics_update(delta: float) -> void:
+func physics_update(delta: float) -> void:
 	pass
--- state_machine.gd
+++ state_machine.gd
@@ -12,20 +12,20 @@ func _ready() -> void:
 			state.state_changed.connect(_on_state_changed)
 		else:
 			push_warning("Invalid node! Should be of type State!")
-	current_state._enter()
+	current_state.enter()
 	
 func _process(delta) -> void:
-	current_state._update(delta)
+	current_state.update(delta)
 	
 func _physics_process(delta) -> void:
-	current_state._physics_update(delta)
+	current_state.physics_update(delta)
 			
 func _on_state_changed(new_state: StringName) -> void:
 	var next_state = states.get(new_state)
 	
 	if next_state and next_state != current_state:
-		current_state._exit()
-		next_state._enter()
+		current_state.exit()
+		next_state.enter()
 		current_state = next_state
 	else:
 		push_error(("Invalid state: '%s'" % new_state))
