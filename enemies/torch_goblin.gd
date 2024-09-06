24c983cc6613e4731f7f7785cc95de2601c9dc91
Jacob Howell <jhowell45@googlemail.com>
2024-09-06 01:22:29 +0100

    disable hitboxes.

--- enemies/torch_goblin.gd
+++ enemies/torch_goblin.gd
@@ -27,7 +27,7 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	_handle_sprite_flip()
-	#_disable_hitboxes()
+	_disable_hitboxes()
 	if attacking:
 		var new_position = player.global_position
 		nav.target_position = new_position
