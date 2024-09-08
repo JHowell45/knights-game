88063e9ffb2fdd7cba39bc55f437f3a325d378d7
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 12:54:12 +0100

    Clean up where the state is grabbed from.

--- label.gd
+++ label.gd
@@ -3,4 +3,5 @@ extends Label
 @onready var state = %CharacterState
 
 func _physics_process(delta: float) -> void:
-	text = state.States.find_key(state.state)
+	#text = state.States.find_key(state.state)
+	pass
