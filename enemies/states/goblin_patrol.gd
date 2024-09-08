508666852d284cc7789446fd218ec1341f5bc313
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 23:30:34 +0100

    get random point to move to.

--- enemies/states/goblin_patrol.gd
+++ enemies/states/goblin_patrol.gd
@@ -1,7 +1,12 @@
 class_name GoblinPatrol extends GoblinState
 
 func enter(_state: StringName, _data := {}) -> void:
-	pass
+	var area: Rect2 = goblin.patrol_zone.get_children()[0].shape.get_rect()
+	var start = area.position
+	var end = area.end
+	
+	var random_point: Vector2 = Vector2(randf_range(start[0], end[0]), randf_range(start[1], end[1]))
+	print(random_point)
 	
 func exit() -> void:
 	pass
@@ -10,7 +15,8 @@ func update(_delta: float) -> void:
 	pass
 	
 func physics_update(_delta: float) -> void:
-	goblin.animator.play("Idle")
+	goblin.animator.play("Run")
+	transition.emit(IDLE)
 
 func handle_input(_event: InputEvent) -> void:
 	pass
