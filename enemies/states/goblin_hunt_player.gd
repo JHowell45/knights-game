38fa4a290c7120905b4e6973d0060a3b68d26ce4
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 00:28:32 +0100

    pass the player node, not the global position.

--- enemies/states/goblin_hunt_player.gd
+++ enemies/states/goblin_hunt_player.gd
@@ -1,8 +1,9 @@
 class_name GoblinHuntPlayer extends GoblinState
 
+@onready var player: Player
 
-func enter(_state: StringName, _data := {}) -> void:
-	pass
+func enter(_state: StringName, data := {}) -> void:
+	player = data["player"]
 	
 func exit() -> void:
 	pass
@@ -11,7 +12,12 @@ func update(_delta: float) -> void:
 	pass
 	
 func physics_update(delta: float) -> void:
-	pass
+	goblin.nav.set_target_position(player.global_position)
+	if not goblin.nav.is_navigation_finished():
+		goblin.animator.play("Run")
+		var next_path = goblin.nav.get_next_path_position()
+		goblin.velocity = (next_path - goblin.global_position).normalized() * goblin.speed * delta
+		goblin.move_and_slide()
 
 func handle_input(_event: InputEvent) -> void:
 	pass
