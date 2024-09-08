86f7110eb393db03fa73cb043bf01391bcbbb679
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 22:21:07 +0100

    Update type check and root name.

--- enemies/states/goblin_state.gd
+++ enemies/states/goblin_state.gd
@@ -2,9 +2,9 @@ class_name GoblinState extends State
 
 const IDLE = "PlayerIdle"
 
-var player: Player
+var goblin: TorchGoblin
 
 func _ready() -> void:
 	await owner.ready
-	player = owner as Player
-	assert(player != null, "The PlayerState state type must be used only in the player scene. It needs the owner to be a Player node.")
+	goblin = owner as TorchGoblin
+	assert(goblin != null, "The PlayerState state type must be used only in the goblin scene. It needs the owner to be a Player node.")
