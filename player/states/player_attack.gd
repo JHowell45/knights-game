008ab5e48338898b982cf0267bcbc8cf76208499
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 18:57:07 +0100

    setup attack to swap based on where the character is pointing.

--- player/states/player_attack.gd
+++ player/states/player_attack.gd
@@ -1,6 +1,7 @@
 class_name PlayerAttack extends PlayerState
 
 @onready var direction: Vector2
+@onready var attack_switch: bool = false
 
 func enter(_state: StringName, _data := {}) -> void:
 	pass
@@ -12,11 +13,27 @@ func update(_delta: float) -> void:
 	pass
 	
 func physics_update(_delta: float) -> void:
-	player.animator.play("Idle")
-	direction = Input.get_vector("left", "right", "up", "down")
-	if direction:
-		transition.emit(RUN, {'direction': direction})
-		
+	_handle_attack()
+	await player.animator.animation_finished
+	if Input.get_vector("left", "right", "up", "down").is_zero_approx():
+		transition.emit(IDLE)
+	else:
+		transition.emit(RUN)
 
 func handle_input(_event: InputEvent) -> void:
 	pass
+
+func _handle_attack():
+	var digit := 1
+	match player.direction:
+		player.Direction.UP:
+			digit = 5
+		player.Direction.DOWN:
+			digit = 3
+		player.Direction.LEFT:
+			pass
+		player.Direction.RIGHT:
+			pass
+	digit += int(attack_switch)
+	attack_switch = not attack_switch
+	player.animator.play("Attack_%d" % digit)
