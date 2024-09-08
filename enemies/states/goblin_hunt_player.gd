c5f322b680490def4a503775e90cc7c3092959a3
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 00:06:20 +0100

    Add huntstate script.

---
+++ enemies/states/goblin_hunt_player.gd
@@ -0,0 +1,17 @@
+class_name GoblinHuntPlayer extends GoblinState
+
+
+func enter(_state: StringName, _data := {}) -> void:
+	pass
+	
+func exit() -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+	
+func physics_update(delta: float) -> void:
+	pass
+
+func handle_input(_event: InputEvent) -> void:
+	pass
