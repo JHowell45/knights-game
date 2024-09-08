76cf670c16bde665359ad9cfeb411c0327dac34a
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 18:16:04 +0100

    Keep track of the players direction.

--- player/player.gd
+++ player/player.gd
@@ -12,3 +12,22 @@ class_name Player extends CharacterBody2D
 @onready var followers: Array = []
 
 @onready var fsm: StateMachine = %StateMachine
+
+enum Direction {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+}
+
+@onready var direction: Direction
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("right"):
+		direction = Direction.RIGHT
+	elif Input.is_action_pressed("left"):
+		direction = Direction.LEFT
+	elif Input.is_action_pressed("up"):
+		direction = Direction.UP
+	elif Input.is_action_pressed("down"):
+		direction = Direction.DOWN
