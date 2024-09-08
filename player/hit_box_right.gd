3ad26a1d2944435c260ba400e2a88d7ea5d177b3
Jacob Howell <jhowell45@googlemail.com>
2024-09-08 22:30:34 +0100

    player correctly emits the take daamge for the goblin.

--- player/hit_box_right.gd
+++ player/hit_box_right.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+var player: Player
+
+func _ready() -> void:
+	await owner.ready
+	player = owner
 
 func _on_body_entered(body: Node2D) -> void:
-	print(body)
+	if body.has_signal('take_damage'):
+		body.take_damage.emit(player.attack)
