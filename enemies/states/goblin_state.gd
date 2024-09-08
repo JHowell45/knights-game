63bac13f3b538fe2bdc36d882eedfad4af10fd4c
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 00:17:15 +0100

    Create root function for hunting the player.

--- enemies/states/goblin_state.gd
+++ enemies/states/goblin_state.gd
@@ -10,3 +10,8 @@ func _ready() -> void:
 	await owner.ready
 	goblin = owner as TorchGoblin
 	assert(goblin != null, "The PlayerState state type must be used only in the goblin scene. It needs the owner to be a Player node.")
+
+
+func hunt_player(body: Node2D) -> void:
+	if body is Player:
+		transition.emit(HUNT, {"player_position": body.global_position})
