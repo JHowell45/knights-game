da0652d0cbc9d3c5387c7810f30e3750e5acf4e3
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 00:48:52 +0100

    setting up signals and scripts for the hit boxes.

--- player/hit_box.gd
+++ player/hit_box.gd
@@ -7,5 +7,9 @@ func _ready() -> void:
 	player = owner
 
 func _on_body_entered(body: Node2D) -> void:
+	_take_damage(body)
+
+
+func _take_damage(body: Node2D) -> void:
 	if body.has_signal('take_damage'):
-		body.take_damage.emit(player.attack)
+			body.take_damage.emit(player.attack)
