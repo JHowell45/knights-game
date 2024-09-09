744db3e6cf554bcd7c9b5a4c768b30a7e46bb2f4
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 17:52:41 +0100

    hitbox script.

---
+++ enemies/hit_box.gd
@@ -0,0 +1,15 @@
+class_name HitBox extends Area2D
+
+var goblin: TorchGoblin
+
+func _ready() -> void:
+	await owner.ready
+	goblin = owner
+
+func _on_body_entered(body: Node2D) -> void:
+	_take_damage(body)
+
+
+func _take_damage(body: Node2D) -> void:
+	if body.has_signal('take_damage'):
+			body.take_damage.emit(goblin.attack)
