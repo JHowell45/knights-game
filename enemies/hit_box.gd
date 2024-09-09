e438b62279ce4faa15d44c04f4ab3e91226d5d02
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 18:02:16 +0100

    Update name to prevent conflicts.

--- enemies/hit_box.gd
+++ enemies/hit_box.gd
@@ -1,4 +1,4 @@
-class_name HitBox extends Area2D
+class_name GoblinHitBox extends Area2D
 
 var goblin: TorchGoblin
 
--- player/hit_box.gd
+++ player/hit_box.gd
@@ -1,4 +1,4 @@
-class_name HitBox extends Area2D
+class_name PlayerHitBox extends Area2D
 
 var player: Player
 
