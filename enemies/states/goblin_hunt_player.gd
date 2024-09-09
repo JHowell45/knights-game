dde7c758c9cdce85e0ce03ae39d7a0f6f49ea116
Jacob Howell <jhowell45@googlemail.com>
2024-09-09 13:22:26 +0100

    update hunt path finding.

--- enemies/states/goblin_hunt_player.gd
+++ enemies/states/goblin_hunt_player.gd
@@ -16,8 +16,16 @@ func physics_update(delta: float) -> void:
 	if not goblin.nav.is_navigation_finished():
 		goblin.animator.play("Run")
 		var next_path = goblin.nav.get_next_path_position()
-		goblin.velocity = (next_path - goblin.global_position).normalized() * goblin.speed * delta
+		var new_velocity = goblin.global_position.direction_to(next_path) * goblin.speed * delta
+		if goblin.nav.avoidance_enabled:
+			goblin.nav.set_velocity(new_velocity)
+		else:
+			_on_navigation_agent_2d_velocity_computed(new_velocity)
 		goblin.move_and_slide()
 
 func handle_input(_event: InputEvent) -> void:
 	pass
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	goblin.velocity = safe_velocity
